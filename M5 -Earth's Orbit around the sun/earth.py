8f636945b243d50f0d50da5930f44ec07ba2d2f3
SateGenie2 <maxbillahk@gmail.com>
2025-07-14 11:18:19 +0000

    Added the RK4 method and organised the numerical methods

--- M5 -Earth's Orbit around the sun/earth.py
+++ M5 -Earth's Orbit around the sun/earth.py
@@ -73,10 +73,88 @@ def euler_method(r,v,accn,dt):
      for i in range(1, len(t)):
           r[i] = r[i-1] + v[i-1] * dt
           v[i] = v[i-1] + accn(r[i-1]) *dt  
-
-# Apply the Euler integration on the given conditions 
-euler_method(r,v,accn,dt)
      
+
+# RK4 Integration
+def rk4_method(r, v, accn, dt):
+     """Ordinary differnetial equation(ode)  for position
+     dr/dt =v
+     r_new = r_old  + dt/6 (k1r + 2*k2r +2*k3r +k4r)
+
+     # ode for velecity 
+     dr/dt =a
+     v_new = v_old  + dt/6(k1v +2*k2V +2*k3v +k4v)
+
+     #Parameter
+     #r: empty array for position of size t
+     #v: empty array for velocity of size t
+     #accn: func to calculate the accn at given position
+     #dt: time step for the simulation
+     This function will update the empty arrays for rand v with the simulated data
+     Mrthod to calculate the steps 
+     step1: - 0
+     k1v = accn(r[i-1])
+     k1r = v[i-1]
+
+     step2: - dt/2 using step 1
+     k2v = accn(r[i-1] + k1r *dt/2 )
+     k2r =  v[i-1] + k1v * dt/2
+
+     step3: - dt/2 using step 2
+     k3v =accn(r[i-1] + k2r * dt/2)
+     k3r = v[i-1] + k2v * dt/2
+      
+     step 4 :- dt using step 3
+     k4v = accn (r[i-1] + k3r * dt)
+     k4r = v[i-1] + k3v * dt
+
+     this function will update the empthy arrays for r and v with the simulated data
+
+     """
+
+     for i in range (1 , len(r)): 
+         # step1: - 0
+          k1v = accn(r[i-1])
+          k1r = v[i-1]
+
+          #step2: - dt/2 using step 1
+          k2v = accn(r[i-1] + k1r *dt/2 )
+          k2r =  v[i-1] + k1v * dt/2
+
+          #step3: - dt/2 using step 2
+          k3v =accn(r[i-1] + k2r * dt/2)
+          k3r = v[i-1] + k2v * dt/2
+          
+          #step 4 :- dt using step 3
+          k4v = accn (r[i-1] + k3r * dt)
+          k4r = v[i-1] + k3v * dt
+
+          # update the r and v
+          v[i] = v[i-1]+ dt/6 * (k1v +2*k2v + 2*k3v + k4v )
+
+          r[i] = r[i-1]+ dt/6 * (k1r +2*k2r + 2*k3r + k4r)
+
+def numerical_integration ( r, v, accn, dt, method= 'euler'):
+     """this function will apply the numerical)integratrion based on the method choosen 
+     if the method is euler or rk4, the respective method will be implemented
+     eles it will raise an error or exception
+     Parameters
+     #r: empty array for position of size t
+     #v: empty array for velocity of size t
+     #accn: func to calculate the accn at given position
+     #dt: time step for the simulation"""
+
+     if method.lower()== "euler":
+          euler_method(r,v,accn,dt)
+     elif method.lower()=="rk4":
+          rk4_method(r,v,accn,dt)
+     else:
+          raise Exception (f'You can either choose "euler" or "rk4". Your current input for the method is:- {method}')
+     
+
+#call the numerical integration
+numerical_integration( r, v, accn , dt , method = 'euler')
+
 # Find the point at which Earth is at its Aphelion ( point at ehich the earth is farthest away from the sun)
 sizes = np.array([np.linalg.norm(position) for position in r])
 pos_aphelion= np.max(sizes)
